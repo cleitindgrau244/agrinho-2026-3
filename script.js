d7e33ad69de7b38f3b850dad083739d231cbd573
cleitindgrau244 <carlos.cominesi@escola.pr.gov.br>
2026-06-03 10:59:04 -0300

    .

---
+++ script.js
@@ -0,0 +1,7 @@
+const btnMensagem = document.getElementById("btnMensagem");
+const mensagem = document.getElementById("mensagem");
+
+btnMensagem.addEventListener("click", () => {
+    mensagem.innerHTML =
+        "🌎 Um agro forte depende de solos saudáveis, água preservada e inovação sustentável. Produzir e proteger podem caminhar juntos!";
+});
