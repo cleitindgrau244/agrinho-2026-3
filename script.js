bfc4e8b0fd66bfd2a09e4290170e93e44cb8352e
cleitindgrau244 <carlos.cominesi@escola.pr.gov.br>
2026-06-03 11:12:43 -0300

    ...

--- script.js
+++ script.js
@@ -1,7 +1,41 @@
-const btnMensagem = document.getElementById("btnMensagem");
-const mensagem = document.getElementById("mensagem");
+// Botão Saiba Mais
 
-btnMensagem.addEventListener("click", () => {
-    mensagem.innerHTML =
-        "🌎 Um agro forte depende de solos saudáveis, água preservada e inovação sustentável. Produzir e proteger podem caminhar juntos!";
+const btnTema = document.getElementById("btnTema");
+const temaDetalhado = document.getElementById("temaDetalhado");
+
+btnTema.addEventListener("click", () => {
+
+    temaDetalhado.classList.toggle("oculto");
+
+    temaDetalhado.scrollIntoView({
+        behavior: "smooth"
+    });
+
+});
+
+
+// Botões dos cards
+
+const botoes = document.querySelectorAll(".btnDetalhe");
+
+botoes.forEach(botao => {
+
+    botao.addEventListener("click", () => {
+
+        document.querySelectorAll(".detalhe").forEach(secao => {
+            secao.classList.add("oculto");
+        });
+
+        const alvo = botao.getAttribute("data-alvo");
+
+        const secao = document.getElementById(alvo);
+
+        secao.classList.remove("oculto");
+
+        secao.scrollIntoView({
+            behavior: "smooth"
+        });
+
+    });
+
 });
